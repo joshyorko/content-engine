f87633cd50bc0924248c22d2794274f6cafa4f4b
joshyorko <joshua.yorko@gmail.com>
2025-03-23 21:19:40 +0000

    Refactor navbar templates and enhance project detail and list views
    - Updated guest navbar to include Sign In and Sign Up links, and improved styling.
    - Enhanced logged-in navbar with user dropdown for account management.
    - Redesigned project detail page to include project management and content items sections with improved layout and styling.
    - Revamped project list page to display projects in a grid format with activation options and improved user prompts.
    - Added login and signup templates for user authentication with responsive design.

--- src/items/urls.py
+++ src/items/urls.py
@@ -8,8 +8,11 @@ urlpatterns = [
     path("<int:id>/", views.item_detail_update_view, name='detail'),
     path("<int:id>/upload/", views.item_upload_view, name='upload'),
     path("<int:id>/files/", views.item_files_view, name='files'),
-    re_path(r'^(?P<id>\d+)/files/(?P<name>.*)$', views.item_file_delete_view, name='files_delete'),
+    re_path(r'^(?P<id>\d+)/files/(?P<n>.*)$', views.item_file_delete_view, name='files_delete'),
     path("<int:id>/edit/", views.item_detail_inline_update_view, name='edit'),
     path("<int:id>/delete/", views.item_delete_view, name='delete'),
-    path("create/", views.item_create_view, name='create')
+    path("create/", views.item_create_view, name='create'),
+    # New proxy URLs
+    path("<int:id>/view/<str:filename>", views.serve_s3_file, {'as_attachment': False}, name='view_file'),
+    path("<int:id>/download/<str:filename>", views.serve_s3_file, {'as_attachment': True}, name='download_file'),
 ]
